4a5d971a155d41d94ad9d76ec0031373f50badb0
Bellière Ludovic <xrogaan@gmail.com>
2009-08-19 20:30:51 +0200

    first step to finish issue gh-1

--- application/admin/xmlexport.php
+++ application/admin/xmlexport.php
@@ -5,42 +5,23 @@
  * @license http://opensource.org/licenses/mit-license.php MIT license
  */
 
-$results = $db->fetchAllAsDict2('qid','rid', "SELECT qr.qid, qr.id AS rid, qq.data AS question, qr.data AS response
-FROM quizz_questions AS qq
-    LEFT JOIN quizz_responses AS qr ON (qr.qid = qq.id)");
-$response_points = $db->fetchAllGroupBy('response_id',"SELECT qrp.response_id, qrp.result_id, qrp.points
-FROM quizz_responses_points AS qrp");
 
-$sxe = new SimpleXmlElement('<?xml version="1.0" encoding="UTF-8"?><questions></questions>');
+$results = $db->fetchAllAsDict('id', 'SELECT id, name, description FROM quizz_results');
+$questions = $db->fetchAllAsDict('id', 'SELECT id, data FROM quizz_questions');
+$responses = $db->fetchAllAsDict('id', 'SELECT id, qid, data FROM quizz_responses');
+$points = $db->fetchAllAsDict2('response_id','result_id','SELECT response_id, result_id, points FROM quizz_responses_points');
 
-$nextQuestion = true;
-foreach ($results as $qid => $question_data) {
-    foreach ($question_data as $rid => $response_data) {
-        if ($nextQuestion) {
-            $question = $sxe->addChild('question');
-            $question->addChild('id',$qid);
-            $question->addChild('data',$response_data['question']);
-            $nextQuestion = false;
-            $responses = $question->addChild('responses');
-        }
-        $response = $responses->addChild('response');
-        $response->addChild('id',$rid);
-        $response->addChild('qid',$qid);
-        $response->addChild('data',$response_data['response']);
-        $pointsData = $response->addChild('pointsData');
-        foreach ($response_points[$rid] as $point_data) {
-            $point = $pointsData->addChild('point');
-            $point->addChild('response_id', $rid);
-            $point->addChild('result_id', $point_data['result_id']);
-            $point->addChild('points', $point_data['points']);
-        }
-    }
-    $nextQuestion = true;
-}
+$export = array(
+    'results' => $results,
+    'questions' => $questions,
+    'responses' => $responses,
+    'points' => $points
+);
 
-header('Content-Type: text/xml');
-$doc = new DOMDocument('1.0');
-$doc->preserveWhiteSpace = false;
-$doc->loadXML( $sxe->asXML());
-$doc->formatOutput = true;
-echo $doc->saveXML();
+require 'spyc-0.4.2/spyc.php';
+
+$x = Spyc::YAMLDump($export);
+header('Content-Type: text/yaml');
+header('Content-Disposition: attachment; filename="quizz-data.yml"');
+echo $x;
+
